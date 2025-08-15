a82239a8ffd00a5fcc437ba432e9712f758e31c0
Tarcisio Oliveira <t@rcis.io>
2025-08-15 21:50:58 +0000

    update

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,19 @@
 package config
 
+import (
+	"os"
+)
+
 type (
 	Config struct {
 		ServerAddress string
 	}
 )
+
+func getEnv(key, defaultValue string) string {
+	value, found := os.LookupEnv(key)
+	if found {
+		return value
+	}
+	return defaultValue
+}
