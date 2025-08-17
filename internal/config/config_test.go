6d032e4db677f7c786ea6973f513c7e24ef1088f
Tarcisio Oliveira <t@rcis.io>
2025-08-17 23:25:01 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -18,6 +18,22 @@ func TestNew(t *testing.T) {
 			wantServerAddress: serverAddressDefault,
 			wantError:         false,
 		},
+		{
+			name: "should create a new Config instance with custom server address: localhost:8081",
+			envValues: map[string]string{
+				serverAddressKey: "localhost:8081",
+			},
+			wantServerAddress: "localhost:8081",
+			wantError:         false,
+		},
+		{
+			name: "should create a new Config instance with custom server address: 127.0.0.1:3000",
+			envValues: map[string]string{
+				serverAddressKey: "127.0.0.1:3000",
+			},
+			wantServerAddress: "127.0.0.1:3000",
+			wantError:         false,
+		},
 	}
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
