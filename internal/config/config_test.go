9293cd939880a98ab7f1e54cd9e94b99e1add632
Tarcisio Oliveira <t@rcis.io>
2025-08-17 01:53:16 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,10 +10,13 @@ func TestNew(t *testing.T) {
 		os.Unsetenv(serverAddressKey)
 		config, err := New()
 		if err != nil {
+			t.Fatalf("New() error=%v", err)
 		}
 		if config == nil {
+			t.Fatal("New() *Config=nil")
 		}
 		if config.ServerAddress != serverAddressDefault {
+			t.Errorf("New().ServerAddress got=%s want=%s", config.ServerAddress, serverAddressDefault)
 		}
 	})
 }
