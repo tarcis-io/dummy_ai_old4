af13c5ed2a591a836332d8bd11f208093d9633a6
Tarcisio Oliveira <t@rcis.io>
2025-08-17 00:50:56 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,8 +1,19 @@
 package config
 
 import (
+	"os"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
+	t.Run("should create a new Config with default values", func(t *testing.T) {
+		os.Unsetenv(serverAddressKey)
+		config, err := New()
+		if err != nil {
+		}
+		if config == nil {
+		}
+		if config.ServerAddress != serverAddressDefault {
+		}
+	})
 }
