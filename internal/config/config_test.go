2e81c874cea3d8f7d4bd27c3eefb672a72b3882f
Tarcisio Oliveira <t@rcis.io>
2025-08-17 03:43:11 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -10,28 +10,28 @@ func TestNew(t *testing.T) {
 	t.Run("should create a new Config instance with default values", func(t *testing.T) {
 		os.Unsetenv(serverAddressKey)
 		config, err := New()
-		if err != nil {
-			t.Fatalf("New() error=%v", err)
-		}
 		if config == nil {
 			t.Fatal("New() *Config=nil")
 		}
 		if config.ServerAddress != serverAddressDefault {
 			t.Errorf("New().ServerAddress got=%s want=%s", config.ServerAddress, serverAddressDefault)
 		}
+		if err != nil {
+			t.Errorf("New() error=%v", err)
+		}
 	})
 	t.Run("should create a new Config instance with custom values", func(t *testing.T) {
 		serverAddress := "localhost:8081"
 		t.Setenv(serverAddressKey, serverAddress)
 		config, err := New()
-		if err != nil {
-			t.Fatalf("New() error=%v", err)
-		}
 		if config == nil {
 			t.Fatal("New() *Config=nil")
 		}
 		if config.ServerAddress != serverAddress {
 			t.Errorf("New().ServerAddress got=%s want=%s", config.ServerAddress, serverAddress)
+		}
+		if err != nil {
+			t.Errorf("New() error=%v", err)
 		}
 	})
 }
