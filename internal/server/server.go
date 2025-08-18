7c4fe29e3b2aedb21f43acbec22236ef00739115
Tarcisio Oliveira <t@rcis.io>
2025-08-18 04:50:30 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,16 @@
 package server
 
+import (
+	"net/http"
+)
+
 type (
 	Server struct {
+		address string
+		handler http.Handler
 	}
 )
+
+func (s *Server) ListenAndServe() error {
+	return http.ListenAndServe(s.address, s.handler)
+}
