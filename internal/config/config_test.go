e87ff71b5b798c3cc1d87133aefa248e0dac7f6c
Tarcisio Oliveira <t@rcis.io>
2025-08-18 02:20:58 +0000

    update

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -49,11 +49,17 @@ func TestNew(t *testing.T) {
 				t.Setenv(key, value)
 			}
 			config, err := New()
-			if (err != nil) != tc.wantError {
-				t.Fatalf("New() error=%v", err)
-			}
 			if tc.wantError {
+				if err == nil {
+					t.Fatal("New() error=nil")
+				}
+				if config != nil {
+					t.Fatal("New() *Config!=nil")
+				}
 				return
+			}
+			if err != nil {
+				t.Fatalf("New() error=%v", err)
 			}
 			if config == nil {
 				t.Fatal("New() *Config=nil")
