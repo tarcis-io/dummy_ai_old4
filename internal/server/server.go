f4330384ec8e9fe1c805274d27225369af344e94
Tarcisio Oliveira <t@rcis.io>
2025-08-19 16:54:51 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,15 +2,27 @@ package server
 
 import (
 	"net/http"
+	"text/template"
 )
 
 type (
 	Server struct {
-		address string
-		handler *http.ServeMux
+		address      string
+		handler      *http.ServeMux
+		pageTemplate *template.Template
+	}
+
+	pageData struct {
+		Title    string
+		WASMPath string
 	}
 )
 
-func (s *Server) ListenAndServe() error {
-	return http.ListenAndServe(s.address, s.handler)
+func (server *Server) ListenAndServe() error {
+	return http.ListenAndServe(server.address, server.handler)
+}
+
+func (server *Server) renderPage(responseWriter http.ResponseWriter, pageData pageData) error {
+	responseWriter.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	return server.pageTemplate.Execute(responseWriter, pageData)
 }
