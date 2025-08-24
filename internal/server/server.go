84063bbc4e860b975d7e6eade0199e6a8e277d6b
Tarcisio Oliveira <t@rcis.io>
2025-08-24 00:05:50 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -32,6 +32,7 @@ func (server *Server) homeHandler(responseWriter http.ResponseWriter, request *h
 	}
 	err := server.renderPage(responseWriter, homePageData)
 	if err != nil {
+		server.logger.Error("Failed to render home page", "error", err)
 		server.error500Handler(responseWriter, request)
 	}
 }
@@ -39,6 +40,7 @@ func (server *Server) homeHandler(responseWriter http.ResponseWriter, request *h
 func (server *Server) aboutHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	err := server.renderPage(responseWriter, aboutPageData)
 	if err != nil {
+		server.logger.Error("Failed to render about page", "error", err)
 		server.error500Handler(responseWriter, request)
 	}
 }
@@ -47,6 +49,7 @@ func (server *Server) error404Handler(responseWriter http.ResponseWriter, reques
 	responseWriter.WriteHeader(http.StatusNotFound)
 	err := server.renderPage(responseWriter, error404PageData)
 	if err != nil {
+		server.logger.Error("Failed to render 404 page", "error", err)
 		server.error500Handler(responseWriter, request)
 	}
 }
@@ -55,6 +58,7 @@ func (server *Server) error500Handler(responseWriter http.ResponseWriter, _ *htt
 	responseWriter.WriteHeader(http.StatusInternalServerError)
 	err := server.renderPage(responseWriter, error500PageData)
 	if err != nil {
+		server.logger.Error("Failed to render 500 page", "error", err)
 		http.Error(responseWriter, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
 }
