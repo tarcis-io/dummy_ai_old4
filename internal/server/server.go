a3cbf173198b9ca9b2a49bd770c2e373dbdc0d23
Tarcisio Oliveira <t@rcis.io>
2025-08-24 15:26:32 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,26 @@
 package server
+
+type (
+	pageData struct {
+		Title    string
+		WASMPath string
+	}
+)
+
+const (
+	titleDefault  = "DummyAI"
+	homeWASMPath  = "/wasm/home.wasm"
+	aboutWASMPath = "/wasm/about.wasm"
+)
+
+var (
+	homePageData = &pageData{
+		Title:    titleDefault,
+		WASMPath: homeWASMPath,
+	}
+
+	aboutPageData = &pageData{
+		Title:    titleDefault,
+		WASMPath: aboutWASMPath,
+	}
+)
