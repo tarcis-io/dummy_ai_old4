e1adfbdbbe85f4d883fab62fdf1ed36ea7b4235a
Tarcisio Oliveira <t@rcis.io>
2025-08-25 14:15:08 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -9,6 +9,20 @@ type (
 
 const (
 	pageDataTitleDefault = "DummyAI"
+	homePagePath         = "GET /"
+	homePageWASMPath     = "/wasm/home.wasm"
+	aboutPagePath        = "GET /about"
+	aboutPageWASMPath    = "/wasm/about.wasm"
+)
+
+var (
+	homePageData  = newPageData(homePageWASMPath)
+	aboutPageData = newPageData(aboutPageWASMPath)
+
+	pageRoutes = map[string]*pageData{
+		homePagePath:  homePageData,
+		aboutPagePath: aboutPageData,
+	}
 )
 
 func newPageData(wasmPath string) *pageData {
