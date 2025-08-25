f28f369127cf861df47f4fddfe4689758d63ce68
Tarcisio Oliveira <t@rcis.io>
2025-08-25 16:30:03 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"embed"
 	"net/http"
+	"text/template"
 )
 
 type (
 	Server struct {
-		address  string
-		serveMux *http.ServeMux
+		address      string
+		serveMux     *http.ServeMux
+		pageTemplate *template.Template
 	}
 
 	pageData struct {
@@ -32,12 +35,20 @@ var (
 		homePageRoutePath:  homePageData,
 		aboutPageRoutePath: aboutPageData,
 	}
+
+	//go:embed web/*
+	webFS embed.FS
 )
 
 func New(address string) (*Server, error) {
+	pageTemplate, err := template.ParseFS(webFS, "web/template/*.html")
+	if err != nil {
+		return nil, err
+	}
 	server := &Server{
-		address:  address,
-		serveMux: http.NewServeMux(),
+		address:      address,
+		serveMux:     http.NewServeMux(),
+		pageTemplate: pageTemplate,
 	}
 	return server, nil
 }
