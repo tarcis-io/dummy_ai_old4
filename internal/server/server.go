a4ecde145c01f222ea71ce45ef5069fd7d93688d
Tarcisio Oliveira <t@rcis.io>
2025-08-27 01:18:58 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,26 @@
 package server
+
+type (
+	Server struct {
+		address string
+	}
+
+	pageData struct {
+		Title    string
+		WASMPath string
+	}
+)
+
+func New(address string) (*Server, error) {
+	server := &Server{
+		address: address,
+	}
+	return server, nil
+}
+
+func newPageData(wasmPath string) *pageData {
+	pageData := &pageData{
+		WASMPath: wasmPath,
+	}
+	return pageData
+}
