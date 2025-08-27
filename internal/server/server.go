e944a1accec422f13b7dac1dabfb74349a7ff5e9
Tarcisio Oliveira <t@rcis.io>
2025-08-27 19:59:48 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,20 @@
 package server
+
+type (
+	pageData struct {
+		Title    string
+		WASMPath string
+	}
+)
+
+const (
+	pageTitleDefault = "DummyAI"
+)
+
+func newPageData(wasmPath string) *pageData {
+	pageData := &pageData{
+		Title:    pageTitleDefault,
+		WASMPath: wasmPath,
+	}
+	return pageData
+}
