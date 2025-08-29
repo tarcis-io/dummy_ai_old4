203b77de54f6159fe9d4372567e23d2189ed344b
Tarcisio Oliveira <t@rcis.io>
2025-08-29 21:16:36 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,14 @@
 package server
+
+type (
+	Server struct {
+		address string
+	}
+)
+
+func New(address string) (*Server, error) {
+	server := &Server{
+		address: address,
+	}
+	return server, nil
+}
