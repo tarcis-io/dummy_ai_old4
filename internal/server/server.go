3954f16335dd6721b3bb90c7154d39f5b43a7235
Tarcisio Oliveira <t@rcis.io>
2025-08-30 04:39:31 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"embed"
+	"io/fs"
 	"log/slog"
 	"net/http"
 )
@@ -13,12 +15,30 @@ type (
 	}
 )
 
+const (
+	staticDirectory = "web/static"
+	staticPath      = "/static/"
+)
+
+var (
+	//go:embed web/*
+	webFS embed.FS
+)
+
 func New(address string, logger *slog.Logger) (*Server, error) {
+	staticFS, err := fs.Sub(webFS, staticDirectory)
+	if err != nil {
+		return nil, err
+	}
+	staticFileServer := http.FileServer(http.FS(staticFS))
+	router := http.NewServeMux()
+	router.Handle(staticPath, http.StripPrefix(staticPath, staticFileServer))
 	if logger == nil {
 		logger = slog.Default()
 	}
 	server := &Server{
 		address: address,
+		router:  router,
 		logger:  logger,
 	}
 	return server, nil
