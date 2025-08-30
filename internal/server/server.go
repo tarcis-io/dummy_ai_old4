9d038525e0a2e657d09b5f28ef7d3db0bf939b64
Tarcisio Oliveira <t@rcis.io>
2025-08-30 06:13:13 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,10 +1,12 @@
 package server
 
 import (
+	"bytes"
 	"embed"
 	"io/fs"
 	"log/slog"
 	"net/http"
+	"text/template"
 )
 
 type (
@@ -13,16 +15,35 @@ type (
 		router  *http.ServeMux
 		logger  *slog.Logger
 	}
+
+	pageData struct {
+		Title    string
+		WASMPath string
+	}
 )
 
 const (
-	staticDirectory = "web/static"
-	staticPath      = "/static/"
+	staticDirectory   = "web/static"
+	staticPath        = "/static/"
+	templatesPattern  = "web/template/*.html"
+	pageTitleDefault  = "DummyAI"
+	homePagePath      = "GET /"
+	homePageWASMPath  = "/static/wasm/home.wasm"
+	aboutPagePath     = "GET /about"
+	aboutPageWASMPath = "/static/wasm/about.wasm"
 )
 
 var (
 	//go:embed web/*
 	webFS embed.FS
+
+	homePageData  = newPageData(homePageWASMPath)
+	aboutPageData = newPageData(aboutPageWASMPath)
+
+	pageRoutes = map[string]*pageData{
+		homePagePath:  homePageData,
+		aboutPagePath: aboutPageData,
+	}
 )
 
 func New(address string, logger *slog.Logger) (*Server, error) {
@@ -33,6 +54,20 @@ func New(address string, logger *slog.Logger) (*Server, error) {
 	staticFileServer := http.FileServer(http.FS(staticFS))
 	router := http.NewServeMux()
 	router.Handle(staticPath, http.StripPrefix(staticPath, staticFileServer))
+	pageTemplates, err := template.ParseFS(webFS, templatesPattern)
+	if err != nil {
+		return nil, err
+	}
+	for pagePath, pageData := range pageRoutes {
+		var buffer bytes.Buffer
+		err = pageTemplates.Execute(&buffer, pageData)
+		if err != nil {
+			return nil, err
+		}
+		router.HandleFunc(pagePath, func(w http.ResponseWriter, r *http.Request) {
+			w.Write(buffer.Bytes())
+		})
+	}
 	if logger == nil {
 		logger = slog.Default()
 	}
@@ -46,4 +81,12 @@ func New(address string, logger *slog.Logger) (*Server, error) {
 
 func (server *Server) Start() error {
 	return http.ListenAndServe(server.address, server.router)
+}
+
+func newPageData(wasmPath string) *pageData {
+	pageData := &pageData{
+		Title:    pageTitleDefault,
+		WASMPath: wasmPath,
+	}
+	return pageData
 }
