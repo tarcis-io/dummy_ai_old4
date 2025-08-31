e1fb154b74fb3df164b24d77204a98c205024df8
Tarcisio Oliveira <t@rcis.io>
2025-08-31 23:17:01 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,14 +1,20 @@
 package server
 
+import (
+	"net/http"
+)
+
 type (
 	Server struct {
 		address string
+		router  *http.ServeMux
 	}
 )
 
 func New(address string) (*Server, error) {
 	server := &Server{
 		address: address,
+		router:  http.NewServeMux(),
 	}
 	return server, nil
 }
