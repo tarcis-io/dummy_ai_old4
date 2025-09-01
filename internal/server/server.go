0e4ec89947038a6e6b50ec958942c59cba211390
Tarcisio Oliveira <t@rcis.io>
2025-09-01 04:31:14 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,29 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type (
+	Server struct {
+		address string
+		router  *http.ServeMux
+	}
+)
+
+func New(address string) (*Server, error) {
+	server := &Server{
+		address: address,
+		router:  http.NewServeMux(),
+	}
+	return server, nil
+}
+
+func (server *Server) Start() error {
+	err := http.ListenAndServe(server.address, server.router)
+	if err != nil {
+		return fmt.Errorf("failed to start server address=%s error=%w", server.address, err)
+	}
+	return nil
+}
