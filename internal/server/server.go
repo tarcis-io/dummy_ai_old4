85a33118aa6ed26076287b4508e88e5f945317a8
Tarcisio Oliveira <t@rcis.io>
2025-09-01 18:48:15 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,2 +1,20 @@
 // Package server provides a pre-configured HTTP server for the application.
 package server
+
+import (
+	"net/http"
+)
+
+type (
+	// Server represents the pre-configured HTTP server for the application.
+	Server struct {
+		address string
+		router  *http.ServeMux
+	}
+
+	// pageData represents the data that is passed to the HTML template.
+	pageData struct {
+		Title    string
+		WASMPath string
+	}
+)
