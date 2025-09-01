5e598ac3fa018b3b4034124e17dfd066ba8b7196
Tarcisio Oliveira <t@rcis.io>
2025-09-01 04:46:44 +0000

    update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,9 @@
 package server
 
 import (
+	"embed"
 	"fmt"
+	"io/fs"
 	"net/http"
 )
 
@@ -12,10 +14,24 @@ type (
 	}
 )
 
+const (
+	staticFilesDirectory = "web/static"
+	staticFilesPath      = "/static/"
+)
+
+var (
+	//go:embed web/*
+	webFS embed.FS
+)
+
 func New(address string) (*Server, error) {
 	server := &Server{
 		address: address,
 		router:  http.NewServeMux(),
+	}
+	err := server.registerStaticFiles()
+	if err != nil {
+		return nil, err
 	}
 	return server, nil
 }
@@ -25,5 +41,14 @@ func (server *Server) Start() error {
 	if err != nil {
 		return fmt.Errorf("failed to start server address=%s error=%w", server.address, err)
 	}
+	return nil
+}
+
+func (server *Server) registerStaticFiles() error {
+	staticFiles, err := fs.Sub(webFS, staticFilesDirectory)
+	if err != nil {
+		return fmt.Errorf("failed to open static files directory error=%w", err)
+	}
+	server.router.Handle(staticFilesPath, http.StripPrefix(staticFilesPath, http.FileServerFS(staticFiles)))
 	return nil
 }
